f8032b3a5c75236d4fb568a81187da76012986ac
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-12-17 21:55:26 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""
+This setup.py file contains configurations for installing and setting up the project.
+"""
+
+from setuptools import find_packages, setup
+
+
+def get_requirements():
+    """
+    This function will return a list of requirements
+    """
+
+    with open("requirements.txt", encoding='utf-8') as f:
+        lines = f.read()
+
+    list_packages = lines.split()
+
+    return list_packages
+
+
+setup(
+    name="finance",
+    version="0.0.1",
+    author="Prathamesh Mohite",
+    author_email="prathamesh.mohite96@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
